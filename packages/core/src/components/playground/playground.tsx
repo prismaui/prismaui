84c85303134240324020c3ee4e079b785b06d0c7
mehrabix <mehrabi@post.com>
2023-06-03 14:01:55 +0330

    test toast

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -6,22 +6,34 @@ import { Component, h, Host, State } from '@stencil/core';
   shadow: true,
 })
 export class Playground {
-  @State() isToastOpen: boolean = false;
+  @State() isToastOpen1: boolean = false;
+  @State() isToastOpen2: boolean = false;
 
-  toggleToast = () => {
-    this.isToastOpen = !this.isToastOpen;
+  toggleToast1 = () => {
+    this.isToastOpen1 = !this.isToastOpen1;
+    this.isToastOpen2 = !this.isToastOpen2;
   }
 
-  handleToastClosed = () => {
-    this.isToastOpen = false;
+  handleToastClosed1 = () => {
+    this.isToastOpen1 = false;
   }
+
+  toggleToast2 = () => {
+    this.isToastOpen1 = !this.isToastOpen1;
+  }
+
+  handleToastClosed2 = () => {
+    this.isToastOpen1 = false;
+  }
+
 
   render() {
     return (
       <Host>
-        <prm-button round="md" size="xl" onClick={this.toggleToast}>Test</prm-button>
+        <prm-button round="md" size="xl" onClick={this.toggleToast1}>Test</prm-button>
 
-        <prm-toast onClose={this.handleToastClosed} show={this.isToastOpen} position="top-left" timer={5000} animation="slide-bottom" variant="success" size="lg" message="This is a success toast!"></prm-toast>
+        <prm-toast onClose={this.handleToastClosed1} show={this.isToastOpen1} position="top-left" timer={5000} animation="slide-bottom" variant="success" size="lg" message="This is a success toast!"></prm-toast>
+        <prm-toast onClose={this.handleToastClosed2} show={this.isToastOpen2} position="top-left" timer={5000} animation="slide-bottom" variant="success" size="lg" message="This is a success toast!"></prm-toast>
 
       </Host>
     );
