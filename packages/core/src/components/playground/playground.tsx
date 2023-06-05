c338ea9f1869fe520c7f58dbb616a432cbcf3bb0
mehrabix <mehrabi@post.com>
2023-06-05 15:10:16 +0330

    refactor css

--- packages/core/src/components/playground/playground.tsx
+++ packages/core/src/components/playground/playground.tsx
@@ -8,6 +8,7 @@ import { Component, h, Host, State } from '@stencil/core';
 export class Playground {
   @State() isToastOpen1: boolean = false;
   @State() isToastOpen2: boolean = false;
+  @State() isPopupOpen:boolean = false
 
   toggleToast1 = () => {
     this.isToastOpen1 = !this.isToastOpen1;
@@ -26,14 +27,27 @@ export class Playground {
     this.isToastOpen2 = false;
   }
 
+  togglePopup = () =>{
+    this.isPopupOpen = !this.isPopupOpen
+  }
+
+  closePopup = () =>{
+    this.isPopupOpen = false
+  }
+
 
   render() {
     return (
       <Host>
-        <prm-button round="md" size="xl" onClick={this.toggleToast1}>Test</prm-button>
+        <prm-button round="md" size="xl" onClick={this.toggleToast1}>Test Toast</prm-button>
+        <prm-button variant='secondary' round="md" size="xl" onClick={this.togglePopup}>Test popup</prm-button>
 
         <prm-toast onClose={this.handleToastClosed1} show={this.isToastOpen1} position="top-left" timer={5000} animation="slide-bottom" variant="success" size="lg" message="This is a success toast!"></prm-toast>
         <prm-toast onClose={this.handleToastClosed2} show={this.isToastOpen2} position="top-left" timer={5000} animation="slide-bottom" variant="success" size="lg" message="This is a success toast!"></prm-toast>
+
+        <prm-popup size='full' isOpen={this.isPopupOpen}>
+          <prm-button onClick={this.closePopup} variant='danger'>Closepopup</prm-button>
+        </prm-popup>
 
       </Host>
     );
