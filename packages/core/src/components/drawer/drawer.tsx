b42cbca548e1319d4b2f278ea239ea4bcd97ef56
mehrabix <mehrabi@post.com>
2023-06-16 18:34:33 +0330

    drawer close fix

--- packages/core/src/components/drawer/drawer.tsx
+++ packages/core/src/components/drawer/drawer.tsx
@@ -3,19 +3,27 @@ import { Component, h, Prop, Event, EventEmitter, State } from '@stencil/core';
 @Component({
   tag: 'prm-drawer',
   styleUrl: 'drawer.scss',
-  shadow: true
+  shadow: true,
 })
 export class Drawer {
   @Prop({ reflect: true }) variant: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info' | 'light' | 'dark' = 'primary';
   @Prop({ reflect: true }) animation: 'fade' | 'scale' | 'slide-top' | 'slide-bottom' | 'slide-left' | 'slide-right' = 'fade';
   @Event({ eventName: 'close' }) onClose: EventEmitter<void>;
-  @State() isOpen: boolean = true;
+  @State() isOpen: boolean = false;
+
+  connectedCallback() {
+    this.isOpen = true;
+  }
+
+  closeDrawer() {
+    this.isOpen = false;
+    this.onClose.emit();
+  }
 
   render() {
     return (
-      <div class='drawer-overlay'>
-        <div
-          class={`drawer-content ${this.animation} ${this.isOpen ? 'open' : 'close'} ${this.variant}`}>
+      <div class={`drawer-overlay ${this.isOpen ? '' : 'close'}`}>
+        <div class={`drawer-content ${this.animation} ${this.isOpen ? 'open' : 'close'} ${this.variant}`}>
           <slot />
         </div>
       </div>
